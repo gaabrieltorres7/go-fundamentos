0ad06f014423caa7203ac9544a266786b2284d1c
Gabriel Torres <gaabrieltt7@gmail.com>
2024-08-02 21:30:29 -0300

    Packaging cmd e math

---
+++ packaging/1/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gaabrieltorres7/packaging/1/math"
+)
+
+func main() {
+	m := math.NewMath(1, 2)
+	m.C = 3
+	fmt.Println(m.C)
+	// fmt.Println(m.Add())
+	// fmt.Println(math.X)
+}
---
+++ packaging/1/go.mod
@@ -0,0 +1,3 @@
+module github.com/gaabrieltorres7/packaging/1
+
+go 1.21.3
